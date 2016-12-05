91ae4dd715d6442a4cd060cbb827bd054d2a9e4b
Stella Canivenc <scanivenc@gmail.com>
2016-12-05 12:48:20 +0000

    Added modified devise for stripe

---
+++ app/controllers/users/registrations_constroller.rb
@@ -0,0 +1,15 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+    
+    def create # override the def create in the devise/registrationsConstroller file
+        super do |resource|
+            if params[:plan]
+                resource.plan.id = params[:plan]
+                if resource.plan_id == 2
+                    resource.save_with_payement
+                else
+                    resource.save
+                end
+            end
+        end
+    end
+end
