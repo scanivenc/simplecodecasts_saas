99c097d1d1b67a9f0bc3e0a37b5f8fdc5819caf2
Stella Canivenc <scanivenc@gmail.com>
2016-12-06 16:16:23 +0000

    Added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,4 +4,20 @@ class ProfilesController < ApplicationController
         @user = User.find( params[:user_id]) #determines who is logged in
         @profile = @user.build_profile #method available because of established model association btw users and profiles
     end
+    
+    def create
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile(profile_params) #store the content from the form and binds it with the user_id
+        if @profile.save
+            flash[:sucess] = "Profile Updated"
+            redirect_to user_path( params[:user_id] )#path lead to the user show page
+        else
+            render action: :new
+        end
+    end
+    
+    private #whitelist the params
+    def profile_params
+        params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
 end
