e8d1e0c748bd16cb1dfe6e3212b45f089ee16cb7
Stella Canivenc <scanivenc@gmail.com>
2016-12-06 10:54:02 +0000

    Added profile new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,7 @@
 class ProfilesController < ApplicationController
-
+    def new
+        # form where a user can fill out their own profile
+        @user = User.find( params[:user_id]) #determines who is logged in
+        @profile = @user.build_profile #method available because of established model association btw users and profiles
+    end
 end
